be6a67ae94ac44d2bf054169715b42cc3cb2fd98
Logan Warner <logan@true.group>
2020-07-20 13:34:43 +1200

    Increase test coverage

--- test/bloc/player_bloc_test.dart
+++ test/bloc/player_bloc_test.dart
@@ -106,4 +106,125 @@ void main() {
         expect: [PlayerState(0, 0, 0, 1)],
       );
   });
+
+  group('Button clicks with increased strength get expected values', () {
+      PlayerBloc playerBloc;
+      setUp(() {
+          testing = true;
+          playerBloc = PlayerBloc();
+      });
+      blocTest('strengthLevel 1 gets expected results',
+        build: () async {
+          playerBloc.setTestState(PlayerState(0, 1, -1, 0));
+          return playerBloc;
+        },
+        act: (bloc) async {
+          for(int i = 0; i < 5; i++) {
+            bloc.add(PlayerEvent.ButtonClicked);
+          }
+        },
+        expect: [
+          PlayerState(2, 1, -1, 0),
+          PlayerState(4, 1, -1, 0),
+          PlayerState(6, 1, -1, 0),
+          PlayerState(8, 1, -1, 0),
+          PlayerState(10, 1, -1, 0),
+        ]
+      );
+
+      blocTest('strengthLevel 1 starting from a large prime gets expected results',
+        build: () async {
+          playerBloc.setTestState(PlayerState(3877, 1, -1, 0));
+          return playerBloc;
+        },
+        act: (bloc) async {
+          for(int i = 0; i < 5; i++) {
+            bloc.add(PlayerEvent.ButtonClicked);
+          }
+        },
+        expect: [
+          PlayerState(3879, 1, -1, 0),
+          PlayerState(3881, 1, -1, 0),
+          PlayerState(3883, 1, -1, 0),
+          PlayerState(3885, 1, -1, 0),
+          PlayerState(3887, 1, -1, 0),
+        ]
+      );
+
+      blocTest('strengthLevel 9 gets expected results',
+        build: () async {
+          playerBloc.setTestState(PlayerState(0, 9, -1, 0));
+          return playerBloc;
+        },
+        act: (bloc) async {
+          for(int i = 0; i < 5; i++) {
+            bloc.add(PlayerEvent.ButtonClicked);
+          }
+        },
+        expect: [
+          PlayerState(10, 9, -1, 0),
+          PlayerState(20, 9, -1, 0),
+          PlayerState(30, 9, -1, 0),
+          PlayerState(40, 9, -1, 0),
+          PlayerState(50, 9, -1, 0),
+        ]
+      );
+      blocTest('strengthLevel 9 gets expected results when starting from large prime',
+        build: () async {
+          playerBloc.setTestState(PlayerState(8513, 9, -1, 0));
+          return playerBloc;
+        },
+        act: (bloc) async {
+          for(int i = 0; i < 5; i++) {
+            bloc.add(PlayerEvent.ButtonClicked);
+          }
+        },
+        expect: [
+          PlayerState(8523, 9, -1, 0),
+          PlayerState(8533, 9, -1, 0),
+          PlayerState(8543, 9, -1, 0),
+          PlayerState(8553, 9, -1, 0),
+          PlayerState(8563, 9, -1, 0),
+        ]
+      );
+      
+
+      blocTest('strengthLevel 1023 gets expected results',
+        build: () async {
+          playerBloc.setTestState(PlayerState(0, 1023, -1, 0));
+          return playerBloc;
+        },
+        act: (bloc) async {
+          for(int i = 0; i < 5; i++) {
+            bloc.add(PlayerEvent.ButtonClicked);
+          }
+        },
+        expect: [
+          PlayerState(1024, 1023, -1, 0),
+          PlayerState(2048, 1023, -1, 0),
+          PlayerState(3072, 1023, -1, 0),
+          PlayerState(4096, 1023, -1, 0),
+          PlayerState(5120, 1023, -1, 0),
+        ]
+      );
+
+      blocTest('strengthLevel 1023 gets expected results when starting from large prime',
+        build: () async {
+          playerBloc.setTestState(PlayerState(6263, 1023, -1, 0));
+          return playerBloc;
+        },
+        act: (bloc) async {
+          for(int i = 0; i < 5; i++) {
+            bloc.add(PlayerEvent.ButtonClicked);
+          }
+        },
+        expect: [
+          PlayerState(7287, 1023, -1, 0),
+          PlayerState(8311, 1023, -1, 0),
+          PlayerState(9335, 1023, -1, 0),
+          PlayerState(10359, 1023, -1, 0),
+          PlayerState(11383, 1023, -1, 0),
+        ]
+      );
+  });
 }
